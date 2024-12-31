a79fa078efc3385db2469047de6b199012b40b78
Raed Sawan <raedswanit@gmail.com>
2024-12-31 13:19:31 +0300

    build edit task screen

--- lib/app/pages/tsks_screens/edit_task_screen/edit_task_screen.dart
+++ lib/app/pages/tsks_screens/edit_task_screen/edit_task_screen.dart
@@ -1,14 +1,60 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:get/get.dart';
-import 'package:maps/app/pages/tsks_screens/edit_task_screen/edit_task_screen_controller.dart';
 
-class EditTaskScreen extends GetView<EditTaskScreenController>{
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+ import 'package:maps/app/data/colors.dart';
+import 'package:maps/app/pages/components/ui/buttons/primary_button.dart';
+import 'package:maps/app/pages/components/ui/fields/text_field.dart';
+ import 'package:maps/app/pages/tsks_screens/edit_task_screen/edit_task_screen_controller.dart';
+
+class EditTaskScreen extends GetView<EditTaskScreenController> {
   const EditTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-  return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: AppColors.card,
+        ),
+        title: const Text(
+          "Add New Task ",
+          style: TextStyle(
+            color: AppColors.card,
+            shadows: [
+              Shadow(
+                color: Colors.black,
+                offset: Offset(0, 2),
+                blurRadius: 0,
+              ),
+            ],
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 17.w, vertical: 39.h),
+        child: Form(
+          key: controller.formKey,
+          child: Column(
+            children: [
+              TextFieldComponent(
+                label: "Task Name * ",
+                controller: controller.tasNameController,
+                validator: (data)=>data!.isEmpty?"Task Name Required":null,
+              ),
+              14.verticalSpace,
+              TextFieldComponent(
+                label: "Task Description ",
+                controller: controller.tasDescriptionController,
+                isMultiLine: true,
+              ),
+              Spacer(),
+              PrimaryButton(label: "save",onPressed: ()=>controller.save())
+            ],
+          ),
+        ),
+      ),
+    );
   }
-
 }
--- lib/app/pages/tsks_screens/edit_task_screen/edit_task_screen_controller.dart
+++ lib/app/pages/tsks_screens/edit_task_screen/edit_task_screen_controller.dart
@@ -1,5 +1,34 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+import 'package:maps/app/data/enums/task_status_enum.dart';
+import 'package:maps/app/data/models/task_model.dart';
+import 'package:maps/app/services/tasks_serveice.dart';
+import 'package:uuid/uuid.dart';
 
-class EditTaskScreenController extends GetxController{
+class EditTaskScreenController extends GetxController {
+  final TaskModel task = Get.arguments['task'];
+  final TextEditingController tasNameController = TextEditingController();
+  final TextEditingController tasDescriptionController =
+      TextEditingController();
+  final GlobalKey<FormState> formKey = GlobalKey();
 
+  @override
+  void onInit() {
+    tasNameController.text = "${task.name}";
+    tasDescriptionController.text = "${task.info}";
+    super.onInit();
+  }
+
+  void save() async {
+    if (formKey.currentState!.validate()) {
+      TaskModel task = TaskModel(
+          id: const Uuid().v1(),
+          info: tasDescriptionController.text,
+          status: TaskStatusEnum.schedule,
+          name: tasNameController.text);
+         await TaskService.updateTask(task);
+      Get.back();
+    } else {}
+  }
 }
